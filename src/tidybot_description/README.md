e4ace383828b918e143fa14e1440a2d843df417c
janchen <janchen@umich.edu>
2025-06-12 15:45:35 -0400

    update tidybot_description package

---
+++ src/tidybot_description/README.md
@@ -0,0 +1,5 @@
+# Tidybot Descriptioin
+
+## Overview
+This package contains resources for the tidybot++ model and defines how to spawn the tidybot++ in Gazebo
+
--- src/tidybot_description/launch/description.launch.py
+++ src/tidybot_description/launch/description.launch.py
@@ -1,6 +1,6 @@
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, Command
 from launch_ros.actions import Node
 
 import xacro
@@ -11,15 +11,30 @@ from ament_index_python.packages import get_package_share_directory
 def generate_launch_description():
     ld = LaunchDescription()
     package_dir = get_package_share_directory('tidybot_description')
-    urdf_path = os.path.join(package_dir, 'urdf', 'tidybot.xacro')
 
-    robot_description_content = xacro.process_file(urdf_path).toxml()
+    robot_description_content = DeclareLaunchArgument(
+        'robot_description',
+        default_value=Command([
+                    'xacro ',
+                    PathJoinSubstitution([package_dir, 'urdf', 'tidybot.xacro']), ' '
+                ]),
+        description='Path to the robot description file (Xacro format)'
+    )
+    use_sim_time = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='true',
+        description='Use simulation time if true'
+    )
 
     robot_state_publisher_node = Node(package='robot_state_publisher',
                                       executable='robot_state_publisher',
                                       parameters=[{
-                                          'robot_description': robot_description_content,
-                                      }])
+                                          'robot_description': LaunchConfiguration('robot_description'),
+                                          'use_sim_time': LaunchConfiguration('use_sim_time')
+                                      }],)
 
+    ld.add_action(use_sim_time)
+    ld.add_action(robot_description_content)
     ld.add_action(robot_state_publisher_node)
     return ld
+
