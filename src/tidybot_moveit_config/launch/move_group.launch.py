660351fd1d36d46db5fc266208498831e53725ba
Yuandi Huang <yuandi@umich.edu>
2025-07-01 20:59:35 +0000

    change moveit configs

--- src/tidybot_moveit_config/launch/move_group.launch.py
+++ src/tidybot_moveit_config/launch/move_group.launch.py
@@ -4,4 +4,5 @@ from moveit_configs_utils.launches import generate_move_group_launch
 
 def generate_launch_description():
     moveit_config = MoveItConfigsBuilder("tidybot", package_name="tidybot_moveit_config").to_moveit_configs()
+    moveit_config.robot_description["use_sim_time"] = True
     return generate_move_group_launch(moveit_config)
