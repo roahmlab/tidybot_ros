52ffc1b8cc483dfd4445cbf906197e103ec40500
Yuandi Huang <yuandi@umich.edu>
2025-07-01 20:59:35 +0000

    change moveit configs

--- src/tidybot_moveit_config/launch/demo.launch.py
+++ src/tidybot_moveit_config/launch/demo.launch.py
@@ -1,29 +1,46 @@
-from moveit_configs_utils import MoveItConfigsBuilder
-from moveit_configs_utils.launches import generate_demo_launch
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import TimerAction
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import IncludeLaunchDescription, TimerAction
+
+from ament_index_python.packages import get_package_share_directory
+from pathlib import Path
+import os, yaml
 
 def generate_launch_description():
-    moveit_config = (
-        MoveItConfigsBuilder("tidybot", package_name="tidybot_moveit_config")
-        .to_moveit_configs()
-    )
+    launch_package_path = Path(get_package_share_directory("tidybot_moveit_config"))
 
     move_group_node = Node(
         package="moveit_ros_move_group",
         executable="move_group",
         output="screen",
         parameters=[
-            moveit_config.to_dict(),  
-            {"use_sim_time": True}    
+            {"use_sim_time": True}
         ],
+    )
+    
+    moveit_config_launch = TimerAction(
+        period=3.0,
+        actions=[
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(
+                    str(launch_package_path / "launch/rsp.launch.py")
+                )
+            ),
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(
+                    str(launch_package_path / "launch/move_group.launch.py")
+                )
+            ),
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(
+                    str(launch_package_path / "launch/moveit_rviz.launch.py")
+                )
+            )
+        ]
     )
 
     return LaunchDescription([
         move_group_node,
-        TimerAction(
-            period=3.0, 
-            actions=[generate_demo_launch(moveit_config)]
-        )
+        moveit_config_launch
     ])
